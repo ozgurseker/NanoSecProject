d558661c0273bb33f93932078047b914b52ca8bf
ozgurseker <ozgursheker@gmail.com>
2025-06-27 16:32:33 -0400

    update

--- returns.R
+++ returns.R
@@ -1,6 +1,7 @@
 library(tidyverse)
 library(timeplyr)
 library(tibbletime)
+library(lubridate)
 
 op <- options(digits.secs=9)
 df_futures <- read_csv("Archive/trb_usdt_futures_export.csv") %>% select(-symbol)
@@ -22,5 +23,38 @@ rm(df_trades)
 
 df_sudden <- read_csv("suddenchanges.csv")
 
-df_short <- tibble(time = unique(df$time)) %>% left_join(df_sudden) %>% select(time, sudden_bid_change_s)
-df_sudden %>% select(time) %>% distinct() %>% nrow()
+df_short <- tibble(time = unique(df$time)) %>% left_join(df_sudden) %>% select(time, sudden_bid_change_s) %>%
+  mutate(counter = cumsum(!is.na(sudden_bid_change_s))) %>% filter(counter > lag(counter, 10)) %>% 
+  select(time) %>% left_join(df) %>% left_join(df_sudden)
+
+df_shorter <- df_short
+df_shorter$last_time <- as.POSIXct(rep(NA, nrow(df_shorter)))
+df_shorter$last_time[!is.na(df_shorter$sudden_bid_change_s)] <- as.POSIXct(df_shorter$time[!is.na(df_shorter$sudden_bid_change_s)] + 0.01)
+
+df_shorter <- df_shorter[order(nrow(df_shorter):1),]
+df_shorter <- df_shorter %>% fill(last_time, .direction = "down") %>% filter(time < last_time)
+
+df_shorter <- df_shorter %>% mutate(time_diff = time - lag(time)) %>%
+  mutate(
+    price_spot = (bid_spot + ask_spot)/2,
+    price_futures = (bid_futures + ask_futures)/2,
+    price_change_s = ifelse(time_diff > 0.01, 0, (log(price_spot)-log(lag(price_spot)))*10000) ,
+    price_change_f = ifelse(time_diff > 0.01, 0, (log(price_futures)-log(lag(price_futures)))*10000)
+    )
+
+df_returns <- df_shorter %>% filter(!is.na(price_change_s), !is.na(price_change_f)) %>%
+  tbl_time(index = time) %>% 
+  collapse_by(period = "10 ms", side = "start") %>%
+  group_by(time) %>% 
+  summarise(max_spot_return = max(cumsum(price_change_s), na.rm = T),
+            min_spot_return = min(cumsum(price_change_s), na.rm = T),
+            max_futures_return = max(cumsum(price_change_f), na.rm = T),
+            min_futures_return = min(cumsum(price_change_f), na.rm = T)) 
+
+write_csv(df_returns, "returns.csv")
+
+  
+  
+  
+  
+  
